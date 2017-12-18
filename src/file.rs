a00f15e1c04dd60b71569a4002d20b7f012e052d
Keith Peters <kp@bit-101.com>
2017-12-17 20:34:59 -0500

    file stuff

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,8 @@
 use std::process::Command;
+use std::fs;
+use std::path::Path;
 
+/// Opens a file with it's specified application.
 #[cfg(target_os="macos")]
 pub fn open(path: &str) {
     Command::new("sh")
@@ -9,6 +12,7 @@ pub fn open(path: &str) {
         .expect("couldn't display gif");
 }
 
+/// Opens a file with it's specified application.
 #[cfg(target_os="linux")]
 pub fn open(path: &str) {
     Command::new("sh")
@@ -19,3 +23,25 @@ pub fn open(path: &str) {
 }
 
 // TODO: windows?
+
+/// Creates a directory at `path` if one does not already exist there.
+/// If there is already a directory there and `wipe` is true, it will attempt to remove that directory first.
+/// If a file already exists there, it will probably panic.
+pub fn make_dir(path: &str, wipe: bool) {
+    if Path::new(path).exists() {
+        if wipe {
+            wipe_dir(path);
+            fs::create_dir(path).unwrap();
+        }
+    }
+    else {
+        fs::create_dir(path).unwrap();
+    }
+}
+
+/// Removes a directory if it exists.
+pub fn wipe_dir(path: &str) {
+    if Path::new(path).exists() {
+        fs::remove_dir_all(path).unwrap();
+    }
+}
